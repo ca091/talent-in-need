1d4073d7ac9bfd9de8e09384cd7ce2f0881a084e
caoqi <caoqi@joyme.sg>
2026-09-08 10:56:38 +0800

    feat: add skill - npx skills add https://github.com/nuxt/ui --skill nuxt-ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui'
+  ],
+  sourcemap: false,
+  devServer: {
+    host: '0.0.0.0',
+    port: 3009,
+  },
 })
